ebdf2e50297c767751046155a51b3019b07b23ac
J Greely <dotclue@github.fake>
2013-08-04 20:23:26 -0700

    Initial checkin

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
 yomitori
 ========
 
-tools to generate custom student editions of Japanese texts
+Tools to generate custom student editions of Japanese texts.
+Default output is Kindle-sized PDF files. This is very much
+a work in progress, and no matter how messy it looks, trust
+me that it's far better than the version I've been reading
+novels with for the past two years.
+
+Requirements
+------------
+
+* Perl 5.10.1+ with DBI, DBD::SQLite, Text::MeCab, XML::Twig
+* [TeXLive 2013](http://www.tug.org/texlive/)
+* [MeCab](https://code.google.com/p/mecab/)
+* [Unidic](http://en.sourceforge.jp/projects/unidic/)
+* dviasm.py (distributed with TeXLive)
+* [JMdict and JMnedict](http://www.edrdg.org/)
+
+Tools
+-----
+
+* ytmakedict: convert JMdict and JMnedict into a simple SQLite database
+* ytgloss: add readings and English definitions to a UTF8-encoded text file
+* ytknown: strip out definitions and readings for words the user knows
+* ytruby: convert the embedded readings into proper furigana by stripping
+  out leading, trailing, and interior kana
+* yt2latex: format a document for processing with upLaTeX
+* dvicleanruby: use dviasm.py to strip furigana that appear more than
+  once per page.
+* ytvocab: extract a vocabulary list from a document, incorporating
+  page-number information from the upLaTeX .aux file.
+* ytdegloss: strip all embedded readings and glosses from a file;
+  useful for comparing versions of a document.
+* Yomitori.pm: utility functions
+
+Basic Usage
+-----------
+
+    ytgloss -u userdict.txt foo.txt |
+        ytknown -k known.txt -g gloss.txt > foo.yt
+    ytruby foo.yt | yt2latex > foo.tex
+    uplatex foo.tex
+    ytvocab -t foo.aux foo.yt > foo-vocab.txt
+    dvicleanruby foo.dvi
+    dvipdfmx foo.dvi
+
+TODO
+----
+
+* document the config file and known/gloss/userdict files, with samples
+* add upLaTeX output to ytvocab (reimplement old code)
+* add yt2html with jquery-based glossing (clean up old code)
+* add ab2yt to convert Aozora Bunko markup (clean up old code)
+* add yt2odt (clean up old code)
+* add yt2word (clean up old code)
+* add decent path substitution to readconfig()
+* add additional paper sizes to yt2latex
+* clean up ytmakedict code and output
---
+++ bin/Yomitori.pm
@@ -0,0 +1,154 @@
+package Yomitori;
+
+require 5.10.1;
+use strict;
+use warnings;
+use FindBin qw($RealBin);
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(
+	readconfig
+	%YT
+	$YTREGEXP
+	InKana
+	InFWAN
+	allkana
+	kata2hira
+	hira2kata
+	parsemeta
+	makemeta
+);
+use vars qw($VERSION);
+our $VERSION = '1.00';
+
+use Data::Dumper;
+$Data::Dumper::Indent = 0;
+$Data::Dumper::Useqq = 1;
+$Data::Dumper::Terse = 1;
+{ no warnings 'redefine';
+    sub Data::Dumper::qquote {
+        my $s = shift;
+        return "'$s'";
+    }
+}
+
+my $DEBUG = 0;
+our %YT;
+our $YTREGEXP = '(\{[^{}]*\})';
+
+my %metakeys = qw(
+	G gloss
+	D dictform
+	R dictreading
+	I id
+);
+
+sub readconfig {
+	my $HOME = $ENV{HOME};
+	open(In,"$HOME/.ytrc") or die "$0: $HOME/.ytrc: $!\n";
+	while (<In>) {
+		next if /^\s*$|^\s*#/;
+		chomp;
+		my ($key,$value) = /^(\S+)\s*=\s*(.*)$/;
+		$YT{$key} = $value;
+	}
+	close(In);
+}
+
+sub allkana {
+	return $_[0] =~ /^\p{InKana}+$/;
+}
+
+#character class for regexp
+sub InKana {
+	return <<END;
+3040\t309F
+30A0\t30FF
+END
+}
+
+#character class for full-width alphanum
+sub InFWAN {
+	return <<END;
+FF10\tFF19
+FF21\tFF3A
+FF41\tFF5A
+END
+}
+
+#TODO: convert katakana "ー" to correct hiragana vowel
+sub kata2hira {
+	my $result;
+	foreach my $char (split(//,$_[0])) {
+		if ($char =~ /^\p{InKataKana}+$/ and ord($char) != 0x30fc) {
+			$result .= chr(ord($char)-96);
+		}else{
+			$result .= $char;
+		}
+	}
+	return $result;
+}
+
+sub hira2kata {
+	my $result;
+	foreach my $char (split(//,$_[0])) {
+		if ($char =~ /^\p{InHiraGana}$/) {
+			$result .= chr(ord($char)+96);
+		}else{
+			$result .= $char;
+		}
+	}
+	return $result;
+}
+
+sub parsemeta {
+	my ($meta) = @_;
+	$meta =~ tr/{}//d;
+	my $result = {
+		id => "",
+		word => "",
+		reading => "",
+		dictform => "",
+		dictreading => "",
+		gloss => "",
+	};
+	my $reading;
+	my ($tmp,$rest) = split(/ /,$meta,2);
+	($result->{word},$result->{reading}) = split(/\|/,$tmp,2);
+	$result->{reading} = "" unless defined $result->{reading};
+	while (defined($rest) and $rest ne "") {
+		$rest =~ s/^ //;
+		$rest =~ s/^([DGIR])=(\S+)//;
+		my ($key,$val) = ($1,$2);
+		if ($key eq "G" and $rest ne "") {
+			# only G can contain whitespace or "="
+			$val .= $rest;
+			$rest = "";
+		}
+		$result->{$metakeys{$key}} = $val;
+	}
+	$result->{reading} = $result->{word} unless $result->{reading};
+	return $result;
+}
+
+#new inline metadata format:
+#	{base|reading I=... K=... R=... G=...}
+# G is the only field that can contain whitespace or "=", so it must always
+# be last if it appears.
+#
+sub makemeta {
+	my ($word) = @_;
+	my $result = "{";
+	$result .= $word->{word} if $word->{word};
+	$result .= "|" . $word->{reading} if $word->{reading}
+		and kata2hira($word->{word}) ne kata2hira($word->{reading});
+	$result .= " D=" . $word->{dictform} if $word->{dictform};
+	$result .= " R=" . $word->{dictreading} if $word->{dictreading};
+	$result .= " I=" . sprintf("id%06d",$word->{id}) if $word->{id};
+	$result .= " G=" . $word->{gloss} if $word->{gloss};
+	return $result . "}";
+}
+
+1;
+__END__
