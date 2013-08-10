37d99742e536150ccc3bcdca7d72e92175565e17
jgreely <dotclue@github.fake>
2013-08-09 20:02:01 -0700

    ytgloss: add ruby for fluff-with-kanji. add default userdict.csv

--- bin/Yomitori.pm
+++ bin/Yomitori.pm
@@ -58,7 +58,7 @@ sub readconfig {
 			tmpdir => "/tmp",
 			knownwords => "known.txt,known-user.txt",
 			glosswords => "gloss.txt,gloss-user.txt",
-			userdict => "userdict.txt",
+			userdict => "userdict.txt,userdict.csv",
 		);
 	}else{
 		open(In,"$HOME/.ytrc") or die "$0: $HOME/.ytrc: $!\n";
